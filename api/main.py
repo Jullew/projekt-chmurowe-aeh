c886a7e162fca40683c02ef2a72684861687086b
Julian Lewandowski <jullewdevpl@gmail.com>
2024-01-07 20:17:06 +0100

    api endpoint 'new-image' added

---
+++ api/main.py
@@ -0,0 +1,22 @@
+# save this as app.py
+import requests
+from flask import Flask, request
+
+
+UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
+UNSPLASH_KEY = '94jB4CI0IBCmv2R5ZbAvkJlpYQ4OrxFX2k8SZfnKVOs'
+
+app = Flask(__name__)
+
+@app.route("/new-image")
+def new_image():
+    word = request.args.get("query")
+    headers = {'Accept-Version': "v1", 'Authorization': 'Client-ID ' + UNSPLASH_KEY
+        }
+    params = {"query": word}
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+    data = response.json()
+    return data
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050)
